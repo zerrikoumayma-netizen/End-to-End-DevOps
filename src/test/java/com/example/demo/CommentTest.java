da10523fd703aa85dc200177f9c487e72a269bea
OUMAYMA ZERRIK <zerrik.oumayma@ensam-casa.ma>
2026-03-24 00:48:16 +0100

    premier commit

---
+++ src/test/java/com/example/demo/CommentTest.java
@@ -0,0 +1,68 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentTest {
+
+    @Test
+    void prePersist() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getContent() {
+    }
+
+    @Test
+    void setContent() {
+    }
+
+    @Test
+    void getCreatedAt() {
+    }
+
+    @Test
+    void setCreatedAt() {
+    }
+
+    @Test
+    void getPost() {
+    }
+
+    @Test
+    void setPost() {
+    }
+
+    @Test
+    void getAuthor() {
+    }
+
+    @Test
+    void setAuthor() {
+    }
+
+    @Test
+    void getParent() {
+    }
+
+    @Test
+    void setParent() {
+    }
+
+    @Test
+    void getReplies() {
+    }
+
+    @Test
+    void setReplies() {
+    }
+}
